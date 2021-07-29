651ddd945946ebfa69a90d15fa48c316c03f8aa8
LHPL3 <aaronbritt12@googlemail.com>
2021-07-29 15:12:47 +0100

    api and map integration

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,23 +3,24 @@ import firebase from '../Firebase/firebase';
 export const checkGroupExists = (groupName) => {
   const group = firebase.database().ref(`${groupName}`);
 
-  return group.once('value').then((data) => {
-    const response = data.val();
+  return group
+    .once('value')
+    .then((data) => {
+      const response = data.val();
 
-        if (response === null) {
-          return false;
-        } else {
-          return true;
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  
+      if (response === null) {
+        return false;
+      } else {
+        return true;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
-//creates a group if none exists with that name with user and location
-export const createGroup = (groupName, username, latitude, longitude) => {
+//send Data to DB
+export const sendData = (groupName, username, latitude, longitude) => {
   const group = firebase.database().ref(`${groupName}`);
   return group.push({
     heading: 0,
@@ -28,13 +29,13 @@ export const createGroup = (groupName, username, latitude, longitude) => {
     speed: 0
   });
 };
-//add username and location to an existing group
-export const joinGroup = (groupName, username, latitude, longitude) => {
+
+//get Group Data
+export const getGroupData = (groupName) => {
   const group = firebase.database().ref(`${groupName}`);
-  return group.push({
-    heading: 0,
-    position: { latitude, longitude },
-    username: username,
-    speed: 0
+
+  return group.once('value').then((data) => {
+    const response = data.val();
+    return response;
   });
 };
