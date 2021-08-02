72f52766ad8cebb8376a97d1f1f2dc7c1c19460c
rorymcdonnell <rorymcdonnell95@gmail.com>
2021-08-02 10:12:51 +0100

    css update

--- src/components/GroupPage.jsx
+++ src/components/GroupPage.jsx
@@ -3,6 +3,8 @@ import { useContext, useState, useEffect } from "react";
 import NavBar from "./NavigationBar";
 import { Link, useParams } from "react-router-dom";
 import { getGroupData } from "../utils/api";
+import Particle from "react-particles-js";
+import particlesConfig from "../assets/particlesConfig.json";
 
 const GroupPage = () => {
   const { group_slug } = useParams();
@@ -26,6 +28,9 @@ const GroupPage = () => {
 
   return (
     <div className="map-page">
+      <div style={{ position: "absolute" }}>
+        <Particle height="100vh" width="100vw" params={particlesConfig} />
+      </div>
       <NavBar />
       <h2>{group_slug}</h2>
       <h4>Welcome {username}</h4>
@@ -38,12 +43,14 @@ const GroupPage = () => {
           );
         })}
       </ul>
-      <Link to={`/nc-final-project/${group_slug}/ar`}>
-        <button className="menu-buttons">View in AR</button>
-      </Link>
-      <Link to={`/nc-final-project/${group_slug}/map`}>
-        <button className="menu-buttons">View in 2D</button>
-      </Link>
+      <div className="button-container">
+        <Link to={`/nc-final-project/${group_slug}/ar`}>
+          <button className="join-button">View in AR</button>
+        </Link>
+        <Link to={`/nc-final-project/${group_slug}/map`}>
+          <button className="create-button">View in 2D</button>
+        </Link>
+      </div>
     </div>
   );
 };
