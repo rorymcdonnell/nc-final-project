1385b7dfb7fcd374487255a610647e330db8c84f
LHPL3 <aaronbritt12@googlemail.com>
2021-08-03 15:09:49 +0100

    amended ar exit button url

--- src/components/Marker.jsx
+++ src/components/Marker.jsx
@@ -36,7 +36,7 @@ const Marker = ({ groupData }) => {
     let counter = 0;
     const lookupObj = Object.keys(groupData);
 
-    let html = `<a href='http://localhost:3000/nc-final-project/${groupName}'><button class="a-enter-vr-button">Exit</button></a><a-scene vr-mode-ui="enabled: false"><a-camera gps-camera rotation-reader></a-camera>`;
+    let html = `<a href='https://rorymcdonnell.github.io/nc-final-project/#/${groupName}'><button class="a-enter-vr-button">Exit</button></a><a-scene vr-mode-ui="enabled: false"><a-camera gps-camera rotation-reader></a-camera>`;
 
     lookupObj.forEach((member) => {
       html += `<a-box color=${colors[counter]} gps-entity-place="latitude: ${groupData[member].position.latitude}; longitude: ${groupData[member].position.longitude}"></a-box>`;
