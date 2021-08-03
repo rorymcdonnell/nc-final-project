a86731912ebbf6aa7503e0d92d75ae2103e93280
LHPL3 <aaronbritt12@googlemail.com>
2021-08-03 12:22:49 +0100

    working!

--- src/components/Marker.jsx
+++ src/components/Marker.jsx
@@ -7,44 +7,13 @@ const Marker = ({ location, groupData }) => {
     setHTML();
   }, []);
 
-  // const setData = async () => {
-  //   setHTML();
-  // };
-
-  const fakeData = {
-    Aaron: {
-      position: {
-        latitude: 52.6757091,
-        longitude: 1.2314057
-      }
-    },
-    Chris: {
-      position: {
-        latitude: 52.681113599999996,
-        longitude: 1.2320768
-      }
-    },
-    Jammer: {
-      position: {
-        latitude: 52.675612,
-        longitude: 1.231774
-      }
-    },
-    John: {
-      position: {
-        latitude: 52.675417,
-        longitude: 1.232085
-      }
-    }
-  };
-
   const setHTML = () => {
-    const lookupObj = Object.keys(fakeData);
+    const lookupObj = Object.keys(groupData);
 
     let html = `<a href='http://localhost:3000/nc-final-project/${groupName}'><button class="a-enter-vr-button">Exit</button></a><a-scene vr-mode-ui="enabled: false"><a-camera gps-camera rotation-reader></a-camera>`;
 
     lookupObj.forEach((member) => {
-      html += `<a-box color="yellow" gps-entity-place="latitude: ${fakeData[member].position.latitude}; longitude: ${fakeData[member].position.longitude}"></a-box>`;
+      html += `<a-box color="yellow" gps-entity-place="latitude: ${groupData[member].position.latitude}; longitude: ${groupData[member].position.longitude}"></a-box>`;
     });
 
     html += `</a-scene>`;
