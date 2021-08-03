049407ac3b19f171d2e0a20a5925371232b22777
LHPL3 <aaronbritt12@googlemail.com>
2021-08-03 12:06:10 +0100

    ar

--- src/components/Marker.jsx
+++ src/components/Marker.jsx
@@ -4,25 +4,53 @@ const Marker = ({ location, groupData }) => {
   const groupName = localStorage.getItem('groupName');
 
   useEffect(() => {
-    setData();
+    setHTML();
   }, []);
 
-  const setData = async () => {
-    setHTML();
+  // const setData = async () => {
+  //   setHTML();
+  // };
+
+  const fakeData = {
+    Aaron: {
+      position: {
+        latitude: 52.6757091,
+        longitude: 1.2314057
+      }
+    },
+    Chris: {
+      position: {
+        latitude: 52.681113599999996,
+        longitude: 1.2320768
+      }
+    },
+    Jammer: {
+      position: {
+        latitude: 52.675612,
+        longitude: 1.231774
+      }
+    },
+    John: {
+      position: {
+        latitude: 52.675417,
+        longitude: 1.232085
+      }
+    }
   };
 
   const setHTML = () => {
-    const lookupObj = Object.keys(groupData);
+    const lookupObj = Object.keys(fakeData);
 
-    let html = `<a href='http://localhost:3000/nc-final-project/${groupName}'><button class="a-enter-vr-button">Exit</button></a><a-scene vr-mode-ui="enabled: false" embedded><a-camera gps-camera rotation-reader></a-camera>`;
+    let html = `<a href='http://localhost:3000/nc-final-project/${groupName}'><button class="a-enter-vr-button">Exit</button></a><a-scene vr-mode-ui="enabled: false"><a-camera gps-camera rotation-reader></a-camera>`;
 
     lookupObj.forEach((member) => {
-      html += `<a-box color="yellow" gps-entity-place="latitude: ${groupData[member].position.latitude}; longitude: ${groupData[member].position.longitude}">`;
+      html += `<a-box color="yellow" gps-entity-place="latitude: ${fakeData[member].position.latitude}; longitude: ${fakeData[member].position.longitude}"></a-box>`;
     });
 
     html += `</a-scene>`;
     let wrapper = document.getElementById('unbelieveAbleScenes');
     wrapper.innerHTML = html;
+    document.body.appendChild(wrapper);
   };
 
   return <div id="unbelieveAbleScenes"></div>;
