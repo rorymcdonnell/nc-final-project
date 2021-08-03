fa7642633830af1613e0b7a63b0fe5a82a42b12d
LHPL3 <aaronbritt12@googlemail.com>
2021-08-03 09:42:30 +0100

    aframe camera controls

--- src/components/Marker.jsx
+++ src/components/Marker.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { getGroupData } from "../utils/api";
-import { Entity, Scene } from "aframe-react";
+import React, { useEffect, useState } from 'react';
+import { getGroupData } from '../utils/api';
+import { Entity, Scene } from 'aframe-react';
 
 const Marker = ({ location, groupData }) => {
   const [refreshedData, setGroupData] = useState(groupData);
-  const groupName = localStorage.getItem("groupName");
+  const groupName = localStorage.getItem('groupName');
 
   useEffect(() => {
     initialSetData();
@@ -14,7 +14,7 @@ const Marker = ({ location, groupData }) => {
   const refreshData = async () => {
     setInterval(async () => {
       await initialSetData();
-      document.getElementById("arjs-video").remove();
+      document.getElementById('arjs-video').remove();
     }, 60000);
   };
 
@@ -26,15 +26,15 @@ const Marker = ({ location, groupData }) => {
   const setHTML = () => {
     const lookupObj = Object.keys(groupData);
 
-    let html = `<a href='http://localhost:3000/nc-final-project/${groupName}'><button class="a-enter-vr-button">Exit</button></a><a-scene embedded
-    "><a-camera minDistance=10 gps-camera rotation-reader></a-camera><div id='members'>`;
+    let html = `<a href='http://localhost:3000/nc-final-project/${groupName}'><button class="a-enter-vr-button">Exit</button></a><a-scene    arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;"
+    "><a-camera arjs-look-controls='smoothingFactor: 0.1' maxDistance=0 minDistance=10 gps-camera gpsMinDistance=10 gpsTimeInterval=5000 rotation-reader></a-camera><div id='members'>`;
 
     lookupObj.forEach((member) => {
       html += `<a-box color="yellow" gps-entity-place="latitude: ${groupData[member].position.latitude}; longitude: ${groupData[member].position.longitude}"></a-box>`;
     });
 
     html += `</div></a-scene>`;
-    let wrapper = document.getElementById("unbelieveAbleScenes");
+    let wrapper = document.getElementById('unbelieveAbleScenes');
     wrapper.innerHTML = html;
   };
 
