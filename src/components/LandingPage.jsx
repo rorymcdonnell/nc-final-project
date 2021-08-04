e52f6a55c8513a980a8caa6c5e39a567c41fb9d0
rorymcdonnell <rorymcdonnell95@gmail.com>
2021-08-04 13:21:17 +0100

    teimout error fixed

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,36 +1,39 @@
-import NavBar from './NavigationBar';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { checkGroupExists, sendData } from '../utils/api';
-import useGeolocation from 'react-hook-geolocation';
+import NavBar from "./NavigationBar";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { checkGroupExists, sendData } from "../utils/api";
+import useGeolocation from "react-hook-geolocation";
 
-import Particle from 'react-particles-js';
-import particlesConfig from '../assets/particlesConfig.json';
+import Particle from "react-particles-js";
+import particlesConfig from "../assets/particlesConfig.json";
 
 const LandingPage = ({ setUsername, setGroupName }) => {
-  const [error, setError] = useState('');
-  const [userCheck, setUserCheck] = useState('');
-  const [groupCheck, setGroupCheck] = useState('');
+  const [error, setError] = useState("");
+  const [userCheck, setUserCheck] = useState("");
+  const [groupCheck, setGroupCheck] = useState("");
   const [groupPageDisabled, setGroupPageDisabled] = useState(true);
 
   const geolocation = useGeolocation();
 
-  const groupName = localStorage.getItem('groupName');
-  const username = localStorage.getItem('username');
+  const groupName = localStorage.getItem("groupName");
+  const username = localStorage.getItem("username");
 
   const checkInputs = async (button) => {
     if (groupCheck.length === 0 || userCheck.length === 0) {
-      setError('Please provide valid inputs');
+      setError("Please provide valid inputs");
+      setTimeout(() => {
+        setError("");
+      }, 3000);
     } else {
       checkGroupExists(groupCheck).then((response) => {
         if (
-          (response && button === 'join') ||
-          (!response && button === 'create')
+          (response && button === "join") ||
+          (!response && button === "create")
         ) {
-          localStorage.setItem('groupName', groupCheck);
-          localStorage.setItem('username', userCheck);
-          setGroupName(localStorage.getItem('groupName'));
-          setUsername(localStorage.getItem('username'));
+          localStorage.setItem("groupName", groupCheck);
+          localStorage.setItem("username", userCheck);
+          setGroupName(localStorage.getItem("groupName"));
+          setUsername(localStorage.getItem("username"));
           sendData(
             groupName,
             username,
@@ -38,10 +41,16 @@ const LandingPage = ({ setUsername, setGroupName }) => {
             geolocation.longitude
           );
           setGroupPageDisabled(false);
-        } else if (button === 'create') {
-          setError('That group exists, please try again');
+        } else if (button === "create") {
+          setError("That group exists, please try again");
+          setTimeout(() => {
+            setError("");
+          }, 3000);
         } else {
           setError("That group doesn't exist, please try again");
+          setTimeout(() => {
+            setError("");
+          }, 3000);
         }
       });
     }
@@ -50,23 +59,23 @@ const LandingPage = ({ setUsername, setGroupName }) => {
   const handleClick = (e) => {
     e.preventDefault();
     switch (e.target.innerHTML) {
-      case 'Create Group':
-        checkInputs('create');
+      case "Create Group":
+        checkInputs("create");
         break;
-      case 'Join a Group':
-        checkInputs('join');
+      case "Join a Group":
+        checkInputs("join");
         break;
       default:
-        return 'Incorrect selection';
+        return "Incorrect selection";
     }
   };
 
   return (
     <div
       className="landing-page"
-      style={{ position: 'relative', overflow: 'hidden' }}
+      style={{ position: "relative", overflow: "hidden" }}
     >
-      <div style={{ position: 'absolute' }}>
+      <div style={{ position: "absolute" }}>
         <Particle height="100vh" width="100vw" params={particlesConfig} />
       </div>
       <NavBar />
